d4bddefbfd06ebbe6ca5f31c2d82ded28a8d7493
Stephane Lafontaine <stephane@medqigong.ca>
2021-06-21 17:44:16 -0400

    Initial Commit

---
+++ Trkr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trkr
+//
+//  Created by Stephane Lafontaine on 2021-06-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trkr/TrkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrkrApp.swift
+//  Trkr
+//
+//  Created by Stephane Lafontaine on 2021-06-21.
+//
+
+import SwiftUI
+
+@main
+struct TrkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
